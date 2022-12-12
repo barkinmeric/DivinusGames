960637125c3ce3d8a24bba31baab24b2b9f3000e
Barkin Meric <barkinmeric@hotmail.com>
2022-12-12 19:34:29 +0300

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,34 +7,30 @@
 					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Divinus Games</span>
 				</NuxtLink>
 			</div>
-			<div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+			<div class="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-2">
 				<div>
-					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Menus</h2>
 					<ul class="text-gray-600 dark:text-gray-400">
 						<li class="mb-4">
-							<a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+							<NuxtLink to="/" class="link hover:underline">Home</NuxtLink>
+						</li>
+						<li class="mb-4">
+							<NuxtLink to="/about" class="link hover:underline">About</NuxtLink>
+						</li>
+						<li class="mb-4">
+							<NuxtLink to="/team" class="link hover:underline">Team</NuxtLink>
 						</li>
 						<li>
-							<a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+							<NuxtLink to="/contact" class="link hover:underline">Contact</NuxtLink>
 						</li>
 					</ul>
 				</div>
-				<div>
-					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-					<ul class="text-gray-600 dark:text-gray-400">
-						<li class="mb-4">
-							<a href="https://github.com/themesberg/flowbite" class="hover:underline">Github</a>
-						</li>
-						<li>
-							<a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-						</li>
-					</ul>
-				</div>
+
 				<div>
 					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
 					<ul class="text-gray-600 dark:text-gray-400">
 						<li class="mb-4">
-							<a href="#" class="hover:underline">Privacy Policy</a>
+							<a href="https://divinusgames.com/privacy-policy" class="hover:underline">Privacy Policy</a>
 						</li>
 						<li>
 							<a href="#" class="hover:underline">Terms &amp; Conditions</a>
@@ -45,9 +41,11 @@
 		</div>
 		<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 		<div class="sm:flex sm:items-center sm:justify-between">
-			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved. </span>
+			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+				>Copyright © 2022 <a href="https://flowbite.com/" class="hover:underline">Divinus Games™</a>. All Rights Reserved.
+			</span>
 			<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-				<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+				<a href="https://facebook.com/divinusgames" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
 					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 						<path
 							fill-rule="evenodd"
@@ -57,7 +55,15 @@
 					</svg>
 					<span class="sr-only">Facebook page</span>
 				</a>
-				<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+				<a href="https://twitter.com/divinus_games" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+						<path
+							d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+						/>
+					</svg>
+					<span class="sr-only">Twitter page</span>
+				</a>
+				<a href="https://instagram.com/divinusgames" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
 					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 						<path
 							fill-rule="evenodd"
@@ -67,33 +73,47 @@
 					</svg>
 					<span class="sr-only">Instagram page</span>
 				</a>
-				<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+				<a href="https://linkedin.com/company/divinus-games" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 49 49" aria-hidden="true" x="0px" y="0px">
 						<path
-							d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+							d="M5.392,0.492C2.268,0.492,0,2.647,0,5.614c0,2.966,2.223,5.119,5.284,5.119c1.588,0,2.956-0.515,3.957-1.489
+						c0.96-0.935,1.489-2.224,1.488-3.653C10.659,2.589,8.464,0.492,5.392,0.492z M7.847,7.811C7.227,8.414,6.34,8.733,5.284,8.733
+						C3.351,8.733,2,7.451,2,5.614c0-1.867,1.363-3.122,3.392-3.122c1.983,0,3.293,1.235,3.338,3.123
+						C8.729,6.477,8.416,7.256,7.847,7.811z"
+						/>
+						<path d="M0.959,45.467h8.988V12.422H0.959V45.467z M2.959,14.422h4.988v29.044H2.959V14.422z" />
+						<path
+							d="M33.648,12.422c-4.168,0-6.72,1.439-8.198,2.792l-0.281-2.792H15v33.044h9.959V28.099c0-0.748,0.303-2.301,0.493-2.711
+						c1.203-2.591,2.826-2.591,5.284-2.591c2.831,0,5.223,2.655,5.223,5.797v16.874h10v-18.67
+						C45.959,16.92,39.577,12.422,33.648,12.422z M43.959,43.467h-6V28.593c0-4.227-3.308-7.797-7.223-7.797
+						c-2.512,0-5.358,0-7.099,3.75c-0.359,0.775-0.679,2.632-0.679,3.553v15.368H17V14.422h6.36l0.408,4.044h1.639l0.293-0.473
+						c0.667-1.074,2.776-3.572,7.948-3.572c4.966,0,10.311,3.872,10.311,12.374V43.467z"
 						/>
 					</svg>
-					<span class="sr-only">Twitter page</span>
+					<span class="sr-only">LinkedIn account</span>
 				</a>
-				<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+				<a href="https://www.youtube.com/channel/UCUzUEGretdDmn8-O8APr8Wg" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 49 49" aria-hidden="true">
 						<path
-							fill-rule="evenodd"
-							d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-							clip-rule="evenodd"
+							d="M39.256,6.5H9.744C4.371,6.5,0,10.885,0,16.274v16.451c0,5.39,4.371,9.774,9.744,9.774h29.512
+			c5.373,0,9.744-4.385,9.744-9.774V16.274C49,10.885,44.629,6.5,39.256,6.5z M47,32.726c0,4.287-3.474,7.774-7.744,7.774H9.744
+			C5.474,40.5,2,37.012,2,32.726V16.274C2,11.988,5.474,8.5,9.744,8.5h29.512c4.27,0,7.744,3.488,7.744,7.774V32.726z"
+						/>
+						<path
+							d="M33.36,24.138l-13.855-8.115c-0.308-0.18-0.691-0.183-1.002-0.005S18,16.527,18,16.886v16.229
+			c0,0.358,0.192,0.69,0.502,0.868c0.154,0.088,0.326,0.132,0.498,0.132c0.175,0,0.349-0.046,0.505-0.137l13.855-8.113
+			c0.306-0.179,0.495-0.508,0.495-0.863S33.667,24.317,33.36,24.138z M20,31.37V18.63l10.876,6.371L20,31.37z"
 						/>
 					</svg>
-					<span class="sr-only">GitHub account</span>
+					<span class="sr-only">Youtube account</span>
 				</a>
-				<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+				<a href="https://www.tiktok.com/@divinus.games" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 256 256" aria-hidden="true">
 						<path
-							fill-rule="evenodd"
-							d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-							clip-rule="evenodd"
+							d="M224,80a52.059,52.059,0,0,1-52-52,4.0002,4.0002,0,0,0-4-4H128a4.0002,4.0002,0,0,0-4,4V156a24,24,0,1,1-34.28418-21.69238,3.99957,3.99957,0,0,0,2.28369-3.61279L92,89.05569a3.99948,3.99948,0,0,0-4.70117-3.938A72.00522,72.00522,0,1,0,172,156l-.00049-42.56348A99.27749,99.27749,0,0,0,224,128a4.0002,4.0002,0,0,0,4-4V84A4.0002,4.0002,0,0,0,224,80Zm-4,39.915a91.24721,91.24721,0,0,1-49.66455-17.1792,4.00019,4.00019,0,0,0-6.33594,3.24707L164,156A64,64,0,1,1,84,94.01223l-.00049,34.271A32.00156,32.00156,0,1,0,132,156V32h32.13184A60.09757,60.09757,0,0,0,220,87.86819Z"
 						/>
 					</svg>
-					<span class="sr-only">Dribbbel account</span>
+					<span class="sr-only">TikTok account</span>
 				</a>
 			</div>
 		</div>
