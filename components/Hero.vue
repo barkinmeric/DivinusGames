e1188fa3aca065cfcea71796e940f7e0ae524342
Barkin Meric <barkinmeric@hotmail.com>
2022-12-13 19:19:26 +0300

    hero responsive design

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,13 +1,13 @@
 <template>
-	<section class="bg-white dark:bg-gray-900">
-		<div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-			<div class="mr-auto place-self-center lg:col-span-7">
-				<h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Divinus Games</h1>
-				<p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+	<section class="bg-gray-900">
+		<div class="relative text-center sm:grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 sm:grid-cols-12">
+			<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:translate-x-0 sm:translate-y-0 sm:static place-self-center sm:col-span-7 text-white">
+				<h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Divinus Games</h1>
+				<p class="max-w-2xl font-light md:text-lg lg:text-xl">
 					We're working hard to bring you the best games you've ever played. Soon, we'll be on every platform and every game category. Stay tuned for future announcements!
 				</p>
 			</div>
-			<div class="lg:mt-0 lg:col-span-5 lg:flex">
+			<div class="max-w-sm mx-auto sm:max-w-xl opacity-20 sm:opacity-100 sm:col-span-5">
 				<img src="/index/banner_bg.webp" alt="mockup" />
 			</div>
 		</div>
