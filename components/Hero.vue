0661db3d6b23296e668b9dac89b364a934aff916
Barkin Meric <barkinmeric@hotmail.com>
2022-12-15 20:56:57 +0300

    aos implementation + scroll animations for hero tags

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,14 +1,14 @@
 <template>
 	<section class="bg-gray-900">
-		<div class="relative text-center sm:grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 sm:grid-cols-12">
-			<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:translate-x-0 sm:translate-y-0 sm:static place-self-center sm:col-span-7 text-white">
-				<h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Divinus Games</h1>
-				<p class="max-w-2xl font-light md:text-lg lg:text-xl">
+		<div class="grid grid-cols-12 grid-rows-1 max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16">
+			<div class="my-auto col-start-2 col-end-12 row-start-1 z-20 sm:col-start-1 sm:col-end-8 place-self-center text-white text-center">
+				<h1 data-aos="fade-right" class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Divinus Games</h1>
+				<p data-aos="fade-right" class="max-w-2xl font-light md:text-lg lg:text-xl">
 					We're working hard to bring you the best games you've ever played. Soon, we'll be on every platform and every game category. Stay tuned for future announcements!
 				</p>
 			</div>
-			<div class="max-w-sm mx-auto sm:max-w-xl opacity-20 sm:opacity-100 sm:col-span-5">
-				<img src="/index/banner_bg.webp" alt="mockup" />
+			<div class="col-start-1 col-end-13 row-start-1 z-10 sm:col-start-8 sm:col-end-13 max-w-sm mx-auto sm:max-w-xl opacity-20 sm:opacity-100">
+				<img src="/index/banner_bg.webp" data-aos="fade-left" />
 			</div>
 		</div>
 	</section>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,11 @@ export default defineNuxtConfig({
 				{ name: "description", content: "Divinus Games" },
 				{ name: "description", content: "Divinus Games" },
 			],
-			link: [{ rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" }],
+			link: [
+				{ rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" },
+				{ rel: "stylesheet", href: "https://unpkg.com/aos@2.3.1/dist/aos.css" },
+			],
+			script: [{ src: "https://unpkg.com/aos@2.3.1/dist/aos.js" }],
 		},
 	},
 });
