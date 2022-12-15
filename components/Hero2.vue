e6e0a4b8981699cb8d91c0a8a919cc09ed112361
Barkin Meric <barkinmeric@hotmail.com>
2022-12-16 01:12:15 +0300

    aos bug partially fixed

--- components/Hero2.vue
+++ components/Hero2.vue
@@ -2,11 +2,11 @@
 	<section class="bg-gray-800 py-12 px-8 sm:py-16 sm:px-12">
 		<div class="grid grid-cols-12 grid-rows-1 max-w-screen-xl sm:gap-8">
 			<div class="col-start-1 col-end-13 row-start-1 z-10 sm:col-start-1 sm:col-end-6 max-w-sm mx-auto sm:max-w-xl opacity-20 sm:opacity-100">
-				<img src="/index/about_img_2.webp" data-aos="fade-right" />
+				<img src="/index/about_img_2.webp" data-aos="fade-right" class="aos-init aos-animate" />
 			</div>
 			<div class="my-auto col-start-2 col-end-12 row-start-1 z-20 sm:col-start-6 sm:col-end-13 text-white text-center">
-				<h1 data-aos="fade-left" class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">What We Do?</h1>
-				<p data-aos="fade-left" class="max-w-2xl font-light md:text-lg lg:text-xl">
+				<h1 data-aos="fade-left" class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl aos-init aos-animate">What We Do?</h1>
+				<p data-aos="fade-left" class="max-w-2xl font-light md:text-lg lg:text-xl aos-init aos-animate">
 					We're a game studio company that develops and publishes mobile (Android & iOS) and PC (Windows) games for multiple categories. We'll be with you on the markets very soon!
 				</p>
 			</div>
--- plugins/aos.client.js
+++ plugins/aos.client.js
@@ -1,3 +1,6 @@
 export default defineNuxtPlugin((nuxtApp) => {
 	AOS.init();
+	nuxtApp.hook("page:start", () => {
+		AOS.refresh();
+	});
 });
